1dc8f64eabf7137b1890e81b30be462fcdc95c9d
anthoni-pivoto <anthonipivoto@gmail.com>
2024-09-27 21:06:30 -0300

    home rendering

--- controller/public.js
+++ controller/public.js
@@ -21,26 +21,18 @@ con.connect(function (err) {
 // con.connect(function (err) {
 //     if (err) throw err;
 //     console.log("Conectado!");
-//     var sql = "CREATE TABLE tb_post (id_post INT AUTO_INCREMENT PRIMARY KEY,id_user int, text VARCHAR(500), tag VARCHAR(30), arquivo varchar(255))";
+//     var sql = "CREATE TABLE tb_post (id_post INT AUTO_INCREMENT PRIMARY KEY,id_user int, text VARCHAR(500), tag VARCHAR(30), arquivo varchar(255), data DATETIME DEFAULT CURRENT_TIMESTAMP)";
 //     con.query(sql, function (err, result) {
 //         if (err) throw err;
 //         console.log("Tabela post criada");
 //     });
 //     if (err) throw err;
 //     console.log("Conectado!");
-//     var sql = "CREATE TABLE tb_user (id_user INT AUTO_INCREMENT PRIMARY KEY, nm_user varchar(255), email VARCHAR(255), pwd_user VARCHAR(255), prof_pic varchar(255))";
+//     var sql = "CREATE TABLE tb_user (id_user INT AUTO_INCREMENT PRIMARY KEY, nm_user varchar(255), email VARCHAR(255), nickname VARCHAR(255), pwd_user VARCHAR(255), prof_pic varchar(255))";
 //     con.query(sql, function (err, result) {
 //         if (err) throw err;
 //         console.log("Tabela user criada");
 //     });
-//     // if (err) throw err;
-//     // console.log("Conectado!");
-//     // var sql = "ALTER TABLE tb_post ADD CONSTRAINT FK_post_user FOREIGN KEY (id_user) REFERENCES tb_user(id_user)";
-//     // con.query(sql, function (err, result) {
-//     //     if (err) throw err;
-//     //     console.log("Constraint Criada");
-//     // });
-
 //     con.end();
 // });
 
@@ -54,8 +46,8 @@ router.get('/login', (req, res) => {
 router.post('/insert-user', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-            var sql = "INSERT INTO tb_user (nm_user, email, pwd_user, prof_pic) VALUES ?";
-            var values = [[fields.name, fields.email, fields.pwd.toString(), files.arquivo[0]]];
+            var sql = "INSERT INTO tb_user (nm_user, email, nickname, pwd_user, prof_pic) VALUES ?";
+            var values = [[fields.name, fields.email,fields.nickname, fields.pwd.toString(), files.arquivo[0]]];
             con.query(sql, [values], function (err, result) {
                 if (err) throw err;
                 console.log("Numero de registros inseridos: " + result.affectedRows);
--- serve.js
+++ serve.js
@@ -1,25 +1,43 @@
 import express from 'express';
+import mysql from 'mysql';
 import publicRoutes from './controller/public.js'
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "node_sup"
+});
+
+con.connect(function (err) {
+  console.log("Conectado!");
+});
 
 app.use('/', publicRoutes);
 
-app.get('/home',function(req,res){
-  res.render('./post/content-home.ejs');
+app.get('/home', function (req, res) {
+  var sql = "select tb_post.text, tb_post.tag, tb_post.data, tb_post.arquivo, tb_user.prof_pic, tb_user.nickname, tb_user.nm_user from tb_post ,tb_user where tb_post.id_user = tb_user.id_user";
+  con.query(sql, function(err, result, fields){
+    if(err){
+      console.log('deu ruim aqui em')
+    };
+    res.render('./post/content-home.ejs', {dadosPost:result});
+  })
 });
 
-app.get('/cadastro', function(req, res){
+app.get('/cadastro', function (req, res) {
   res.render('./user/content-cadastro.ejs');
 })
 
 app.use('/views', express.static('views'));
-      app.get('/views/style.css', (req, res) => {
-        res.setHeader('Content-Type', 'text/css');
-        res.sendFile(__dirname + '/views/style.css');
-      });
-      
+app.get('/views/style.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(__dirname + '/views/style.css');
+});
+
 app.listen(2000, () => console.log("Servidor onlaine"));
